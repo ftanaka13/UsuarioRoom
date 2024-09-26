b9b04e495f1aa6b475157cfbedddd20c9cb73792
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-25 22:18:01 -0300

    UsuarioAdapter implementado

---
+++ app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/adapters/UsuarioAdapter.kt
@@ -0,0 +1,32 @@
+package br.com.faculdadeimpacta.usuarioroom.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import br.com.faculdadeimpacta.usuarioroom.data.models.Usuario
+import br.com.faculdadeimpacta.usuarioroom.databinding.UsuarioItemBinding
+
+class UsuarioAdapter(private val listaUsuario: List<Usuario>) :
+    RecyclerView.Adapter<UsuarioAdapter.UsuarioVH>() {
+
+    inner class UsuarioVH(private val binding: UsuarioItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(usuario: Usuario) {
+            binding.usuario = usuario
+            binding.imageViewFavorito.isEnabled = usuario.favorito
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioVH {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = UsuarioItemBinding.inflate(layoutInflater, parent, false)
+        return UsuarioVH(binding)
+    }
+
+    override fun onBindViewHolder(holder: UsuarioVH, position: Int) {
+        holder.onBind(listaUsuario[position])
+    }
+
+    override fun getItemCount(): Int = listaUsuario.size
+}
