6c721c7b7512b27a2eddcbb0ad13ac2750984e69
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-25 22:25:50 -0300

    layout e binding implementados

--- app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/CriacaoEdicaoFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/CriacaoEdicaoFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import br.com.faculdadeimpacta.usuarioroom.R
+import br.com.faculdadeimpacta.usuarioroom.databinding.FragmentCriacaoEdicaoBinding
 
 class CriacaoEdicaoFragment : Fragment() {
+
+    private var _binding: FragmentCriacaoEdicaoBinding? = null
+    private val binding get() = _binding!!
+    private val args: CriacaoEdicaoFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_criacao_edicao, container, false)
+        _binding = FragmentCriacaoEdicaoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/ListaFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/ListaFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.faculdadeimpacta.usuarioroom.R
+import br.com.faculdadeimpacta.usuarioroom.databinding.FragmentListaBinding
 
 class ListaFragment : Fragment() {
+
+    private var _binding: FragmentListaBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista, container, false)
+        _binding = FragmentListaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
