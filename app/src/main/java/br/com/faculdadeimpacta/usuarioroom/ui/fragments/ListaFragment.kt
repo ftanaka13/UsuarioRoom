9623a86650943c5423aa652f03c775977dc91d17
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-25 22:53:38 -0300

    ListaFragment implementado

--- app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/ListaFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/ListaFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.faculdadeimpacta.usuarioroom.R
 import br.com.faculdadeimpacta.usuarioroom.databinding.FragmentListaBinding
+import br.com.faculdadeimpacta.usuarioroom.ui.activities.MainActivity
+import br.com.faculdadeimpacta.usuarioroom.ui.adapters.UsuarioAdapter
+import br.com.faculdadeimpacta.usuarioroom.ui.viewmodels.UsuarioViewModel
+import br.com.faculdadeimpacta.usuarioroom.ui.viewmodels.UsuarioViewModelFactory
 
 class ListaFragment : Fragment() {
 
     private var _binding: FragmentListaBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: UsuarioViewModel by activityViewModels {
+        UsuarioViewModelFactory((activity as MainActivity).usuarioRepository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +31,22 @@ class ListaFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentListaBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        viewModel.listaUsuarios.observe(viewLifecycleOwner) { lista ->
+            binding.recyclerView.adapter = UsuarioAdapter(lista)
+            binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        }
+
+        viewModel.getLista()
+
+        binding.floatingActionButton.setOnClickListener {
+            val direction = ListaFragmentDirections.actionListaFragmentToCriacaoEdicaoFragment(0)
+            findNavController().navigate(direction)
+        }
+
     }
 }
