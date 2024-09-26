7d327cad0c303ed1e1e7393e79cf79bcc2caafcc
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-25 23:06:14 -0300

    CriacaoEdicaoFragment implementado

--- app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/CriacaoEdicaoFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/usuarioroom/ui/fragments/CriacaoEdicaoFragment.kt
@@ -1,18 +1,27 @@
 package br.com.faculdadeimpacta.usuarioroom.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import br.com.faculdadeimpacta.usuarioroom.R
+import br.com.faculdadeimpacta.usuarioroom.data.models.Usuario
 import br.com.faculdadeimpacta.usuarioroom.databinding.FragmentCriacaoEdicaoBinding
+import br.com.faculdadeimpacta.usuarioroom.ui.activities.MainActivity
+import br.com.faculdadeimpacta.usuarioroom.ui.viewmodels.UsuarioViewModel
+import br.com.faculdadeimpacta.usuarioroom.ui.viewmodels.UsuarioViewModelFactory
 
 class CriacaoEdicaoFragment : Fragment() {
 
     private var _binding: FragmentCriacaoEdicaoBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: UsuarioViewModel by activityViewModels {
+        UsuarioViewModelFactory((activity as MainActivity).usuarioRepository)
+    }
     private val args: CriacaoEdicaoFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -22,5 +31,29 @@ class CriacaoEdicaoFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCriacaoEdicaoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val usuarioId = args.userId
+        if (usuarioId == 0) {
+            binding.buttonSalvar.setOnClickListener {
+                val novoUsuario = Usuario(nome = binding.editTextText.text.toString().trim())
+                viewModel.inserir(novoUsuario)
+            }
+        } else {
+            viewModel.usuario.observe(viewLifecycleOwner) { usuario ->
+                binding.editTextText.setText(usuario?.nome)
+                var usuarioBuscado = usuario
+                binding.buttonSalvar.setOnClickListener {
+                    usuarioBuscado?.nome = binding.editTextText.text.toString().trim()
+                    viewModel.atualizar(usuarioBuscado!!)
+                }
+            }
+            viewModel.getUsuario(usuarioId)
+
+
+        }
     }
 }
